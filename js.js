36e5d3a2f9c7f06afcd4f688d3536ca7cc843aaf
BangarangCrafts <67075011+BangarangCrafts@users.noreply.github.com>
2022-03-10 11:11:27 +0200

    Update js.js

--- js.js
+++ js.js
@@ -517,3 +517,29 @@ function Pollymould(){
 	pollymoulpick.style.display = "block";
 	
 };
+/*Copy to clipboard Function/////////////////////////////////*/
+function copyemail(){
+	var textcopy = document.getElementById("E_Button_P");
+	var currentRange;
+	if(document.getSelection().rangeCount > 0)
+     {
+          currentRange = document.getSelection().getRangeAt(0);
+          window.getSelection().removeRange(currentRange);
+     }
+     else
+     {
+          currentRange = false;
+     } 
+     var CopyRange = document.createRange();
+     CopyRange.selectNode(textcopy);
+     window.getSelection().addRange(CopyRange);
+     document.execCommand("copy");
+ 
+     window.getSelection().removeRange(CopyRange);
+ 
+     if(currentRange)
+     {
+          window.getSelection().addRange(currentRange);
+     }
+	 alert("email copyed to clipboard.");
+};
